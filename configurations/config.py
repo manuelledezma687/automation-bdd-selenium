fb9650375eb07b09f611a459295512d7a4631fc6
manuelledezma687 <manuelledezma687@gmail.com>
2023-01-28 18:01:07 -0300

    pylint

--- configurations/config.py
+++ configurations/config.py
@@ -1,6 +1,3 @@
-
-
-class testData:
-    BASE_URI= 'https://www.demoblaze.com/'
-    USER='user_succesfull_2023'
-    PASSWORD='user123'
+BASE_URI= 'https://www.demoblaze.com/'
+USER='user_succesfull_2023'
+PASSWORD='user123'
--- conftest.py
+++ conftest.py
@@ -4,34 +4,27 @@ import selenium.webdriver
 
 
 @pytest.fixture
-def config(scope='session'):
-
-  with open('config.json') as config_file:
-    config = json.load(config_file)
-  
-  assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
-  assert isinstance(config['implicit_wait'], int)
-  assert config['implicit_wait'] > 0
-
-  return config
+def configuration_opt():
+    with open('config.json',"r",encoding="utf8") as config_file:
+        configuration = json.load(config_file)
+    assert configuration['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+    assert isinstance(configuration['implicit_wait'], int)
+    assert configuration['implicit_wait'] > 0
+    return configuration
 
 
 @pytest.fixture
-def browser(config):
-
-  if config['browser'] == 'Firefox':
-    b = selenium.webdriver.Firefox()
-  elif config['browser'] == 'Chrome':
-    b = selenium.webdriver.Chrome()
-  elif config['browser'] == 'Headless Chrome':
-    opts = selenium.webdriver.ChromeOptions()
-    opts.add_argument('headless')
-    b = selenium.webdriver.Chrome(options=opts)
-  else:
-    raise Exception(f'Browser "{config["browser"]}" is not supported')
-
-  b.implicitly_wait(config['implicit_wait'])
-
-  yield b
-
-  b.quit()
+def browser(configuration_opt):
+    if configuration_opt['browser'] == 'Firefox':
+        browser_opt = selenium.webdriver.Firefox()
+    elif configuration_opt['browser'] == 'Chrome':
+        browser_opt = selenium.webdriver.Chrome()
+    elif configuration_opt['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        browser_opt = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser "{configuration_opt["browser"]}" is not supported')
+    browser_opt.implicitly_wait(configuration_opt['implicit_wait'])
+    yield browser_opt
+    browser_opt.quit()
