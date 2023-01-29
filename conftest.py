a7b47ba0d66134699788dc3c434e050f78059588
manuelledezma687 <manuelledezma687@gmail.com>
2023-01-28 22:19:50 -0300

    pylint

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import selenium.webdriver
 
 
 @pytest.fixture
-def configuration_opt():
+def config():
     with open('config.json',"r",encoding="utf8") as config_file:
         configuration = json.load(config_file)
     assert configuration['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
@@ -14,17 +14,17 @@ def configuration_opt():
 
 
 @pytest.fixture
-def browser(configuration_opt):
-    if configuration_opt['browser'] == 'Firefox':
+def browser(config):
+    if config['browser'] == 'Firefox':
         browser_opt = selenium.webdriver.Firefox()
-    elif configuration_opt['browser'] == 'Chrome':
+    elif config['browser'] == 'Chrome':
         browser_opt = selenium.webdriver.Chrome()
-    elif configuration_opt['browser'] == 'Headless Chrome':
+    elif config['browser'] == 'Headless Chrome':
         opts = selenium.webdriver.ChromeOptions()
         opts.add_argument('headless')
         browser_opt = selenium.webdriver.Chrome(options=opts)
     else:
-        raise Exception(f'Browser "{configuration_opt["browser"]}" is not supported')
-    browser_opt.implicitly_wait(configuration_opt['implicit_wait'])
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
+    browser_opt.implicitly_wait(config['implicit_wait'])
     yield browser_opt
     browser_opt.quit()
